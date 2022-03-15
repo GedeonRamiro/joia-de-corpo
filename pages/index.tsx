7a238a12e65d9a790bd865f31a7f75c3d6eb9495
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-03-15 10:48:45 -0300

    feat: index create caroseul

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,10 +14,31 @@ const Home: NextPage = () => {
        <title>Joia de Corpo</title>
       </Head>
       <Header>
+        
         <div className="container mx-auto">
-            <Image src={bannerBiquini} width="1700" height="429"></Image>
-            <Image src={bannerLingerie} width="1700" height="429"></Image>
-            <Image src={bannerPijama} width="1700" height="429"></Image>
+            <div className="carousel w-full">
+              <div id="slide1" className="carousel-item relative w-full">
+              <Image src={bannerBiquini} width="1550" height="500"></Image>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide3" className="btn btn-circle border-none  bg-pink-500 hover:bg-pink-300">❮</a> 
+                  <a href="#slide2" className="btn btn-circle border-none bg-pink-500 hover:bg-pink-300">❯</a>
+                </div>
+              </div> 
+              <div id="slide2" className="carousel-item relative w-full">
+              <Image src={bannerLingerie} width="1550" height="500"></Image>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide1" className="btn btn-circle border-none bg-pink-500 hover:bg-pink-300">❮</a> 
+                  <a href="#slide3" className="btn btn-circle border-none bg-pink-500 hover:bg-pink-300">❯</a>
+                </div>
+              </div> 
+              <div id="slide3" className="carousel-item relative w-full">
+              <Image src={bannerPijama} width="1550" height="500"></Image>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide2" className="btn btn-circle border-none bg-pink-500 hover:bg-pink-300">❮</a> 
+                  <a href="#slide1" className="btn btn-circle border-none bg-pink-500 hover:bg-pink-300">❯</a>
+                </div>
+              </div> 
+            </div>
         </div>
       </Header>
     </>
