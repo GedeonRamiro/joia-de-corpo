edc12c6fa8b94d8ebee08d82591370012f8525d7
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-03-29 08:33:17 -0300

    feat: create page sobre

--- pages/sobre.tsx
+++ pages/sobre.tsx
@@ -13,11 +13,11 @@ const Company = () => {
             <div className="my-10 md:my-20">
                 <div className="container mx-auto">
                     <div className="flex flex-col-reverse md:flex-row items-center">
-                        <div className="flex flex-col w-full md:w-1/2 justify-center items-start pt-12 pb-24 px-6">
+                        <div className="flex flex-col w-full md:w-1/2 justify-center items-start px-6">
                             <h1 className="font-bold text-3xl my-4">CAT INFO</h1>
-                            <p className="leading-normal mb-4">Os gatos, assim como todos os felinos, foram originados a partir da evolução do miacis, um mamífero predador, que habitou a Terra no Período Paleoceno, há aproximadamente 55 milhões de anos, época em que surgiram os mais antigos mamíferos carnívoros.[21] O primeiro felino identificado por meio de fósseis é o Dinictis, datado de 37 milhões de anos, do qual derivou o proailurus, ancestral comum de todos os gatos, o qual viveu há aproximadamente de 20 milhões de anos.</p>
+                            <p className="leading-normal mb-4 sm:text-base text-sm ">Os gatos, assim como todos os felinos, foram originados a partir da evolução do miacis, um mamífero predador, que habitou a Terra no Período Paleoceno, há aproximadamente 55 milhões de anos, época em que surgiram os mais antigos mamíferos carnívoros.[21] O primeiro felino identificado por meio de fósseis é o Dinictis, datado de 37 milhões de anos, do qual derivou o proailurus, ancestral comum de todos os gatos, o qual viveu há aproximadamente de 20 milhões de anos.</p>
                         </div>
-                        <div className="md:w-1/2 md:py-6 text-center mx-6 sm:mx-0">
+                        <div className="md:w-1/2 md:py-6 text-center mx-4 sm:mx-0">
                             <Image
                                 className="rounded-lg" 
                                 src={imgSobre} 
