6939f08f1ca0d69fb39d8027f5d43d3bd1d13fdc
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-04-01 10:20:58 -0300

    feat: config prismic

---
+++ utils/prismic-configuration.ts
@@ -0,0 +1,11 @@
+import * as prismic from '@prismicio/client'
+
+// Prismic API endpoint
+export const apiEndpoint = 'https://joia-de-corpo.prismic.io/api/v2'
+
+// Access Token if the repository is not public
+// Generate a token in your dashboard and configure it here if your repository is private
+export const accessToken = ''
+
+// Client method to query Prismic
+export const client = prismic.createClient(apiEndpoint, { accessToken });
