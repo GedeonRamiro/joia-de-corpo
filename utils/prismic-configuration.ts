b3d9b39b5ec16887639eae2b69dd5c2ccb834a64
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-05-16 09:18:11 -0300

    feat: Environment Variables

--- utils/prismic-configuration.ts
+++ utils/prismic-configuration.ts
@@ -1,7 +1,7 @@
 import * as prismic from '@prismicio/client';
 
 // Prismic API endpoint
-export const apiEndpoint = String(process.env.REACT_APP_API_URL);
+export const apiEndpoint = String(process.env.NEXT_APP_API_URL);
 // Access Token if the repository is not public
 // Generate a token in your dashboard and configure it here if your repository is private
 export const accessToken = '';
